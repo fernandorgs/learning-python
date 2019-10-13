49eecbf54f3b7fe73a1ec279cb43466e90ecfb24
Machado <52207469+fernandorgs@users.noreply.github.com>
2019-10-13 00:46:35 -0300

    Create 5-loop.py

---
+++ hackerrank/5-loop.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+    range(1,10)
+    for i in list(range(1,11)):
+        print(n,"x",i,"=",n*i)
