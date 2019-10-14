3c8d4077f662b04a0403cd2099aeee7451f50377
Machado <52207469+fernandorgs@users.noreply.github.com>
2019-10-13 23:19:24 -0300

    Update 6-review_challenge.py

--- hackerrank/6-review_challenge.py
+++ hackerrank/6-review_challenge.py
@@ -1,12 +1,18 @@
 #!/bin/python3
 T = int(input())
-for j in list(range(T)):
-    S = list(input())
-    even = ""
-    odd = ""
-    for i in range(len(S)):
-        if (i % 2) == 0:
-            even = even+S[i]
+if (T >= 1) and (T <= 10):
+    for j in list(range(T)):
+        S = list(input())
+        if (len(S) >= 2) and (len(S) <= 10000):
+            even = ""
+            odd = ""
+            for i in range(len(S)):
+                if (i % 2) == 0:
+                    even = even+S[i]
+                else:
+                    odd = odd+S[i]
+            print(even,odd)
         else:
-            odd = odd+S[i]
-    print(even,odd)
+            print("word must have 2-10000 chars.")
+else:
+    print("only 1-10 interations allowed.")
